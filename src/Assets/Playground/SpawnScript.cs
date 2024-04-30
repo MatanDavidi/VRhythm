89df0ff06a1e18b12de9c894a62fd02aa0ce0421
Steven Sbarbaro <sbarbarosteven@gmail.com>
2024-04-30 11:09:17 +0200

    Push *.meta files by Matan <3

---
+++ src/Assets/Playground/SpawnScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    public GameObject prefab; // Assign your prefab in the Unity Editor
+    public Transform spawnPosition;
+    // Function to instantiate the prefab at a given position
+    public void Spawn()
+    {
+        if (prefab != null)
+        {
+            Instantiate(prefab, spawnPosition.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogError("Prefab is not assigned!");
+        }
+    }
+}
